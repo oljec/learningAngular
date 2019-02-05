be692bb5a0c78c3150a0f884614526b527eef78c
Oleg Kreichi <oljeck@gmail.com>
2019-02-05 18:39:31 +0200

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-study-project';
-}
+export class AppComponent {}
